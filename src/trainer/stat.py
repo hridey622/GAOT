small fix: add config.files and add one option for scaling

src/trainer/stat.py
@@ -397,6 +397,8 @@ class StaticTrainer_VX(TrainerBase):
         u_train_flat = u_train.reshape(-1, u_train.shape[-1])
         u_mean = np.mean(u_train_flat, axis=0)
         u_std = np.std(u_train_flat, axis=0) + EPSILON  # Avoid division by zero
+        # u_mean = np.array(0.9530)
+        # u_std = np.array(0.3153)
         self.u_mean = torch.tensor(u_mean, dtype=self.dtype)
         self.u_std = torch.tensor(u_std, dtype=self.dtype)
         u_train = (u_train - u_mean) / u_std
@@ -409,6 +411,8 @@ class StaticTrainer_VX(TrainerBase):
             c_train_flat = c_train.reshape(-1, c_train.shape[-1])
             c_mean = np.mean(c_train_flat, axis=0)
             c_std = np.std(c_train_flat, axis=0) + EPSILON
+            # c_mean = np.array([0.8046, 7.6054, 2.0414])
+            # c_std = np.array([0.3062, 4.3355, 2.4575])
             self.c_mean = torch.tensor(c_mean, dtype=self.dtype)
             self.c_std = torch.tensor(c_std, dtype=self.dtype)
             c_train = (c_train - c_mean) / c_std
@@ -417,7 +421,6 @@ class StaticTrainer_VX(TrainerBase):
             c_train = torch.tensor(c_train, dtype=self.dtype)
             c_val = torch.tensor(c_val, dtype=self.dtype)
             c_test = torch.tensor(c_test, dtype=self.dtype)
-
         # --- Convert to Tensors ---
         # Handle None case for c_train/val/test when converting
         u_train = torch.tensor(u_train, dtype=self.dtype)
@@ -500,7 +503,9 @@ class StaticTrainer_VX(TrainerBase):
         nb_search = NeighborSearch(self.model_config.args.magno.gno_use_open3d)
         gno_radius = self.model_config.args.magno.gno_radius
         scales = self.model_config.args.magno.scales
-        coord_scaler = CoordinateScaler(target_range=(-1, 1))
+        coord_scaler = CoordinateScaler(
+            target_range=(-1, 1),
+            mode = dataset_config.coord_scaling)
         latent_queries = self._generate_latent_queries(
             token_size = self.model_config.latent_tokens_size,
             coord_scaler = coord_scaler

src/utils/scale.py
@@ -18,18 +18,24 @@ def rescale(x:torch.Tensor, lims=(-1,1))->torch.Tensor:
     """
     return (x-x.min()) / (x.max()-x.min()) * (lims[1] - lims[0]) + lims[0]
 
+
 class CoordinateScaler:
-    def __init__(self, target_range = (-1, 1)):
+    def __init__(self, target_range = (-1, 1), mode = "per_dim_scaling"):
         """
         Parameters
         ----------
         target_range: Tuple[float, float]
             The range to scale the coordinates to.
+        mode: str
+            Scaling strategy: 'global_scaling' or 'per_dim_scaling'.
         """
         if not isinstance(target_range, (tuple, list)) or len(target_range) != 2:
             raise ValueError("target_range must be a tuple or list of length 2.")
+        if mode not in ['global_scaling', 'per_dim_scaling']:
+            raise ValueError("mode must be either 'global_scaling' or 'per_dim_scaling'.")
         self.target_range = target_range
         self.out_min, self.out_max = target_range
+        self.mode = mode
     
     def __call__(self, x:torch.Tensor)->torch.Tensor:
         """
@@ -45,19 +51,21 @@ class CoordinateScaler:
 
         if not isinstance(x, torch.Tensor):
             raise ValueError(f"x must be a torch.Tensor. but got {type(x)}")
-        
         if x.numel() == 0:
             raise ValueError("x is empty.")
         if x.ndim != 2:
             raise ValueError("x must be a 2D tensor.")
-        
-        x_min_per_dim = torch.min(x, dim=0, keepdim=True)[0] # Shape: [1, num_dimensions]
-        x_max_per_dim = torch.max(x, dim=0, keepdim=True)[0] # Shape: [1, num_dimensions]
+        if self.mode == 'global_scaling':
+            x_min = x.min()
+            x_max = x.max()
+        elif self.mode == 'per_dim_scaling':
+            x_min = torch.min(x, dim=0, keepdim=True)[0] # Shape: [1, num_dimensions]
+            x_max = torch.max(x, dim=0, keepdim=True)[0] # Shape: [1, num_dimensions]
 
-        x_range_per_dim = x_max_per_dim - x_min_per_dim + EPSILON
-        normalized_01 = (x - x_min_per_dim) / x_range_per_dim
+        x_range = x_max - x_min + EPSILON
+        normalized = (x - x_min) / x_range
 
-        scaled_x = normalized_01 * (self.out_max - self.out_min) + self.out_min
+        scaled_x = normalized * (self.out_max - self.out_min) + self.out_min
         
         return scaled_x
 